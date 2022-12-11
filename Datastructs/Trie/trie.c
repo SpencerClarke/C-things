6d448b17b903f14334deac2e13d765b78432fcd2
Spencer-Clarke <sdc2@njit.edu>
2022-12-10 21:44:52 -0500

    Added feature to get highest value in trie

--- Datastructs/Trie/trie.c
+++ Datastructs/Trie/trie.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <limits.h>
 struct Node
 {
     int child_count;
@@ -13,6 +13,7 @@ struct Node
     char value;
      
     struct Node **children;
+    struct Node *parent;
 };
 struct Trie
 {
@@ -23,6 +24,8 @@ struct Trie create_trie(void);
 void add_word(struct Trie *trie, char *word, int number);
 int index_of(struct Trie *trie, char *word);
 void delete(struct Trie *trie, char *word);
+struct Node *_get_highest_node(struct Node *current, int current_max);
+int get_highest_value(struct Trie *trie, char *buffer, int buff_size);
 void _destroy(struct Node *current);
 void destroy(struct Trie *trie);
 
@@ -30,6 +33,7 @@ int main(int argc, char **argv)
 {
     struct Trie trie;
     char word[256];
+    char *highest;
     int i;
     FILE *fin;
 
@@ -48,6 +52,10 @@ int main(int argc, char **argv)
     }
     printf("\"%s\": %d\n", argv[2], index_of(&trie, argv[2]));
 
+    highest = malloc(sizeof(char) * 256);
+    printf("Highest value: %d\n", get_highest_value(&trie, highest, 256));
+    printf("Highest word: %s\n", highest);
+
     destroy(&trie);
     fclose(fin);
     return 0;
@@ -61,6 +69,7 @@ struct Trie create_trie(void)
     out.root->children = malloc(sizeof(struct Node * ) * 8);
     out.root->child_size = 1;
     out.root->child_count = 0;
+    out.root->parent = NULL;
 
     return out;
 }
@@ -98,6 +107,7 @@ void add_word(struct Trie *trie, char *word, int number)
             new_node->children = malloc(sizeof(struct Node) * 1);
             new_node->child_count = 0;
             new_node->child_size = 1;
+            new_node->parent = current_node;
             new_node->value = word[i];
             new_node->valid = 0;
             current_node->children[current_node->child_count++] = new_node;
@@ -156,7 +166,6 @@ void delete(struct Trie *trie, char *word)
 
     if(word[0] == '\0')
     {
-        exit(2);
         trie->root->valid = 0;
         return;
     }
@@ -208,6 +217,55 @@ void delete(struct Trie *trie, char *word)
         return;
     }
 }
+struct Node *_get_highest_node(struct Node *current, int current_max)
+{
+    int i;
+    struct Node *true_max;
+    struct Node *test_max;
+
+    if(current->valid)
+    {
+        if(current->number > current_max)
+        {
+            true_max = current;
+            current_max = current->number;
+        }
+    }
+    for(i = 0; i < current->child_count; i++)
+    {
+        test_max = _get_highest_node(current->children[i], INT_MIN);
+        if(test_max->number >= current_max)
+        {
+            current_max = test_max->number;
+            true_max = test_max;
+        }
+    }
+    return true_max;
+}
+int get_highest_value(struct Trie *trie, char *buffer, int buff_size)
+{
+    struct Node *max;
+    int max_num;
+    int i, j;
+
+    if(buff_size < 1)
+    {
+        printf("Insufficient buffer size\n");
+        exit(2);
+    }
+    max = _get_highest_node(trie->root, INT_MIN);
+    max_num = max->number;
+    buffer[0] = '\0';
+    for(i = 0; max->parent != NULL && i < buff_size-1; i++)
+    {
+        for(j = i; j >= 0; j--)
+            buffer[j+1] = buffer[j];
+        buffer[0] = max->value;
+        max = max->parent;
+    }
+    return max_num;
+}
+
 void _destroy(struct Node *current)
 {
     int i;

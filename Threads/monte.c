feb6455467e4929e83d512c79819d8ff181f58a4
Spencer-Clarke <sdc2@njit.edu>
2022-12-13 19:14:28 -0500

    Added integral pi estimation with openmp

--- Threads/monte.c
+++ Threads/monte.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <limits.h>
 #include <math.h>
 #include <pthread.h>
--- Threads/monteomp.c
+++ Threads/monteomp.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <limits.h>
 #include <omp.h>
 
